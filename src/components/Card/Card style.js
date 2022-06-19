cb4c095bc2bdc65f7d2c5571ac370dd1a4a9de05
Yonatan Leibovitch <yonatanleibovitch@Yonatans-MacBook-Pro.local>
2022-06-19 17:11:18 +0300

    card design in proccess

--- src/components/Card/Card style.js
+++ src/components/Card/Card style.js
@@ -4,18 +4,34 @@ const cardStyles = makeStyles({
     root: {
         width: 350,
         height: 230,
-        "& h2": {
-            marginRight: 20,
-            marginTop: 10,
-            fontWeight: "Bold",
-            textAlign: "right",
-           
+        "& h3": {
+            paddingTop: 15,
+            paddingRight: 20,
+            fontWeight:"Bold"
+        },
+        "& p":{
+            paddingRight: 30,
+            paddingTop: 5,
+            fontSize: 18,
+            color:"#8F8F8F"
+
+        },
+        "& img":{
+            position:"relative",
+            bottom: 80,
+            width: "23%",
+            marginLeft: "10px"
+        },
+        "& h4":{
+            display:"inline-block",
+            margin: "0px 5px"
         }
+
     },
-    typography: {
-        paddingRight: 20,
-        paddingTop: 10,
-    }
+    // typography: {
+    //     paddingRight: 20,
+    //     // paddingTop: 10,
+    // }
 });
 
 export default cardStyles;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,18 +3,25 @@ import cardStyles from "./Card style";
 import Paper from "@mui/material/Paper";
 import { Grid } from "@mui/material";
 import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 export default function Card(props) {
   const cardClasses = cardStyles();
   return (
     <Grid item>
       <Paper className={cardClasses.root}>
-        <Typography className={cardClasses.typography} align="right" variant="h4" sx={{ fontWeight: "Bold" }}>
-          ניו יורק
-        </Typography>
-        <Typography className={cardClasses.typography} color="text.secondary" align="right" variant="body1">
-          אובך
-        </Typography>
+          <Typography align="right" variant="h4" component="h3">
+            ניו יורק
+          </Typography>
+          <Typography align="right" component="p">
+            אובך
+          </Typography>
+          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon"/>
+        <Box>
+          <Typography component="h4">טמפ׳ נמדדת</Typography>
+          <Typography component="h4">טמפ׳ נמדדת</Typography>
+          <Typography component="h4">טמפ׳ נמדדת</Typography>
+        </Box>
       </Paper>
     </Grid>
   );
